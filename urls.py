9ac1a8e4a8545bb0cdc86319d283abc1d16c3356
Mustaec R.F <scorpio-84@mail.ru>
2022-12-10 09:04:55 +0500

    fix_problems_lesson_1_homework_add_dinamic_url_for_another _page

--- urls.py
+++ urls.py
@@ -1,11 +1,19 @@
-from datetime import date
+import platform
+from pprint import pprint
+
 from views import Index, AnotherPage
 
 
 # front controller
 def secret_front(environ, request):
-    print(environ['USERNAME'])
-    request['user'] = environ['USERNAME']
+    x = platform.system()
+    if platform.system() == 'Darwin':
+        pprint(environ)
+        print(environ['USER'])
+        request['user'] = environ['USER']
+    elif platform.system() == 'Windows':
+        print(environ['USERNAME'])
+        request['user'] = environ['USERNAME']
 
 
 def other_front(environ, request):
@@ -18,5 +26,5 @@ fronts = [secret_front, other_front]
 routes = {
     '/': Index(),
     '/index.html/': Index(),
-    '/another_page.html/': AnotherPage(),
+    '/another_page/': AnotherPage(),
 }
