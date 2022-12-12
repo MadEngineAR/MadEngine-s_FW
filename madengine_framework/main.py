8de7b80195eb9851014ccc32af0f7c2ff682203b
MadEngineAR <scorpio-84@mail.ru>
2022-12-12 09:53:29 +0500

    homework_lesson_2

--- madengine_framework/main.py
+++ madengine_framework/main.py
@@ -1,4 +1,6 @@
-from pprint import pprint
+from quopri import decodestring
+from termcolor import colored, cprint
+from madengine_framework.FW_requests import PostRequests, GetRequests
 
 
 class PageNotFound404:
@@ -31,6 +33,32 @@ class Framework:
         request = {}
         # наполняем словарь request элементами
         # этот словарь получат все контроллеры
+
+        # Получаем все данные запроса
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+
+        if method == 'POST':
+            data = PostRequests().get_request_params(environ)
+            message = Framework.decode_value(data)["comments"].strip(" ")
+            from_user = Framework.decode_value(data)["name"]
+            from_user_email = Framework.decode_value(data)["email"]
+            request['data'] = Framework.decode_value(data)
+            print(f'Нам пришёл post-запрос: {Framework.decode_value(data)}')
+            cprint(f'Cообщение от: {from_user}', 'blue', 'on_grey',
+                   ['underline'])
+            cprint(f'email: {from_user_email}', 'blue', 'on_grey',
+                   ['underline'])
+            cprint(f'Текст сообщения: {message}', 'blue', 'on_grey',
+                   ['underline'])
+            # cprint(f'Нам сообщение: {Framework.decode_value(data)}', 'green', 'on_blue')
+        if method == 'GET':
+            request_params = GetRequests().get_request_params(environ)
+            request['request_params'] = Framework.decode_value(request_params)
+            if request_params:
+                print(f'Нам пришли GET-параметры:'
+                      f' {Framework.decode_value(request_params)}')
+
         # отработка паттерна front controller
         for front in self.fronts_lst:
             front(environ, request)
@@ -38,3 +66,12 @@ class Framework:
         code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace('%', '=').replace("+", " "), 'UTF-8')
+            val_decode_str = decodestring(val).decode('UTF-8')
+            new_data[k] = val_decode_str
+        return new_data
--- urls.py
+++ urls.py
@@ -1,18 +1,14 @@
 import platform
-from pprint import pprint
+
 
 from views import Index, AnotherPage
 
 
 # front controller
 def secret_front(environ, request):
-    x = platform.system()
     if platform.system() == 'Darwin':
-        pprint(environ)
-        print(environ['USER'])
         request['user'] = environ['USER']
     elif platform.system() == 'Windows':
-        print(environ['USERNAME'])
         request['user'] = environ['USERNAME']
 
 
@@ -25,6 +21,5 @@ fronts = [secret_front, other_front]
 
 routes = {
     '/': Index(),
-    '/index.html/': Index(),
     '/another_page/': AnotherPage(),
 }
